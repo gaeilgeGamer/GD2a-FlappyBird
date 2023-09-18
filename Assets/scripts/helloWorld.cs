d160410393dd05717bd61cb9d51c3ae2090d7ce2
naoise collins <naoisecollins@gmail.com>
2023-09-18 21:12:31 +0100

    Updated vscode

---
+++ Assets/scripts/helloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class helloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello World");
+    }
+}
